39a7db67398fa27d15196eccd2a8ae2e8bf96732
Rafi1072 <rafi110-96@o2.pl>
2024-07-05 11:18:29 +0200

    homework-done

---
+++ task-3.js
@@ -0,0 +1,7 @@
+function getElementWidth(content, padding, border) {
+  return parseInt(content) + 2 * parseInt(padding) + 2 * parseInt(border);
+}
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
